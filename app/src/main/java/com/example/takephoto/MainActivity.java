84277eac8f37c09a2047002c65a77028e4ff3229
David <xldaviduson@gmail.com>
2019-11-16 21:32:04 +0100

    Practica acabada

--- app/src/main/java/com/example/takephoto/MainActivity.java
+++ app/src/main/java/com/example/takephoto/MainActivity.java
@@ -1,18 +1,15 @@
-package com.example.camara;
+package com.example.takephoto;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.takephoto.R;
 
 import java.io.*;
 
@@ -26,11 +23,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // En pirmier lugar, si el directorio al que se apunta no existe, se crea desde cero:
         if (!dir.exists()) {
             dir.mkdir();
         }
-        // Instanacio un objeto de imagen y lo añado como archivo:
         ImageView img = findViewById(R.id.image);
         if (dir.listFiles().length > 0) {
             File image = new File(dir, dir.listFiles()[dir.listFiles().length - 1].getName());
@@ -41,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         Button b = findViewById(R.id.button);
+        // Ensure that there's a camera activity to handle the intent
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,12 +57,9 @@ public class MainActivity extends AppCompatActivity {
             iv.setImageBitmap(imageBitmap);
 
             OutputStream os = null;
-            // Esta funcion lo que hace es recorrer los archivos del directorio y
-            // si no existe, mapea el archivo en una imagen ".PNG" y acaba de
-            // recorrer el directorio.
             try {
                 for (int i = 0; i <= dir.listFiles().length; i++) {
-                    File file = new File(dir, "foto" + i + ".png");
+                    File file = new File(dir, "photo" + i + ".png");
                     if (!file.exists()) {
                         os = new FileOutputStream(file);
                         imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, os);
@@ -74,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             } catch (IOException e) {
-                Log.e("ERROR","No se pudo guardar la imagen");
+                System.out.println("ERROR al guardar la imagen");
             }
         }
     }
