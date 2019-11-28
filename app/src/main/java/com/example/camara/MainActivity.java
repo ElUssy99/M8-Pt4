8f1f9791c63f09da8d0c94ccac6f9a5e9a0bdf28
David Usón <xldaviduson@gmail.com>
2019-11-28 18:47:56 +0100

    Cosas

--- app/src/main/java/com/example/camara/MainActivity.java
+++ app/src/main/java/com/example/camara/MainActivity.java
@@ -1,28 +1,25 @@
 package com.example.camara;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+<<<<<<< HEAD
 import java.io.File;
 import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     ImageView nImageView;
+=======
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+>>>>>>> parent of 84277ea... Practica acabada
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< HEAD
         Button b = findViewById(R.id.button);
 
         b.setOnClickListener(new View.OnClickListener() {
@@ -60,5 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
+=======
+>>>>>>> parent of 84277ea... Practica acabada
     }
 }
